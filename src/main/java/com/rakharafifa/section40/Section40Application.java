7149e38ff01c91036bc6b5cf8238ac14dcce835a
RakhaRafifArifin <rakhara104@gmail.com>
2022-04-25 11:06:47 +0700

    praktikum secion 40

---
+++ src/main/java/com/rakharafifa/section40/Section40Application.java
@@ -0,0 +1,13 @@
+package com.rakharafifa.section40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section40Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakharafifa/section40/Section40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakharafifa.section40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
